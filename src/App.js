3d98319d9065959eed144053dde463c97bd08214
TakashimaY <yousukes53@gmail.com>
2024-08-17 12:46:37 +0900

    created fetchDetection function to make GET request to server URL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+} from '@chakra-ui/react';
+
+const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+const App = () => {
+  const [detection, setDetection] = useState([]);
+
+  useEffect(() => {
+    fetchDetection();
+  }) 
+
+  const fetchDetection = async () => {
+    try {
+      const response = await axios.get(`${serverUrl}`)
+      setDetection(response.data);
+    }catch(error) {
+      console.error('error fetching detection:', error);
+    }
 }
 
-export default App;
+}
