ce91e116d41668ff5a2521bb03a344d6eaef90df
TakashimaY <yousukes53@gmail.com>
2024-08-20 20:26:02 +0900

    Change display order to descending order

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
         console.log('Server URL:', serverUrl);
         const response = await axios.get(`${serverUrl}/detection`);
         console.log('Detection data:', response.data);
-        setDetection(response.data);
+        setDetection(response.data.reverse());
       } catch(error) {
         console.error('error fetching detection:', error);
       }
